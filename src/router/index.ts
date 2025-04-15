b93651ab9652205598c3456e4eb136b3fde697cb
Jeff Walker <jeff@walkerenvres.com>
2025-04-14 22:23:00 -0400

    feat: web hash history mode
    Switched router to hash history to simplify deployment to S3.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ExplorerView from '../views/ExplorerView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
